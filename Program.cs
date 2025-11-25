bb6c96276cbc623131c3407dbc0ce9c9d6c2e6b2
Michael Holmes <boxofyellow@github.com>
2025-11-25 13:48:53 -0500

    skill point

--- Program.cs
+++ Program.cs
@@ -222,7 +222,7 @@ foreach (var reward in rewardCosts.Keys.OrderBy(x => x))
     }
 }
 
-var targetedReward = "Raid Cards";
+var targetedReward = "Skill Points";
 
 // Maximize crafting the target reward
 Console.WriteLine($"\n\n=== OPTIMIZING FOR TARGET REWARD: {targetedReward} ===\n");
